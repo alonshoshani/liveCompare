1b9b4694faedd908da2ee112d3f797f53191259c
alon <alon.shoshani@gmail.com>
2014-07-08 09:08:33 +0300

    get file from php client

--- server.js
+++ server.js
@@ -3,16 +3,25 @@ var express = require('express');
 var app = express();
  
 app.use('/client',express.static(__dirname + '/public'));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+
 
 app.post('/addDelta', function(req, res) {
+	var file=req.files.file;
+	var fileName=file.name;
+	var filePath=file.path;
+	console.log("file name :"+fileName);
+	console.log("file path :"+filePath);
+    res.send("true");
+});
+
+app.get('/addDeltass', function(req, res) {
 	console.log("file");
   //  res.send("file");
 });
-
-
  
 var io = require('socket.io').listen(app.listen(3000));
-
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
